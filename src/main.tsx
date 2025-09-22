bc1633b3346c0040190838e7abc6b23700b65d21
Mark John Allen Nizal <154728620+marknizal@users.noreply.github.com>
2025-09-22 09:21:58 +0800

    setup global css & styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import '../src/styles/global.css'
+import '@ant-design/v5-patch-for-react-19';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
---
+++ src/styles/index.ts
@@ -0,0 +1,32 @@
+export const RADIUS = {
+  sm: "0.5rem",
+  md: "0.75rem",
+  base: "1rem",
+  lg: "2rem",
+  full: "50%",
+};
+
+export const FONTSIZE = {
+  xs: "0.8rem",
+  sm: "0.9rem",
+  base: "1rem",
+  md: "1.25rem",
+  lg: "1.5rem",
+  xl: "1.75rem",
+};
+
+export const BREAKPOINT = {
+  mobile: "@media only screen and (max-width: 767px)",
+  tablet: "@media only screen and (min-width: 768px) and (max-width: 1279px)",
+  desktop: "@media only screen and (min-width: 1280px)",
+};
+
+export const COLOR = {
+  primary: "#1877F2",
+  white: "#FFFFFF",
+  black: "#000000",
+  grey: "#CCCCCC",
+
+  transparent: "rgba(0,0,0,0)",
+  background: "#F2F4F7",
+};
